17d08f173bbafa27fc40c9b7833364f117da5eb5
SikukuGlen <glen.sikuku@strathmore.edu>
2024-09-17 19:51:05 +0300

    Updated structure

---
+++ includes/constants.php
@@ -0,0 +1,11 @@
+<?php
+
+define('DBTYPE', 'PDO');
+define('HOSTNAME', 'localhost:3307');
+define('DBPORT', '3307');
+define('HOSTUSER', 'root');
+define('HOSTPASS', '');
+define('DBNAME', 'ics e');
+
+
+?>
