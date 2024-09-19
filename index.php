1db61b19c3a02f84345a08e2dd297f632fd4b1bc
SikukuGlen <glen.sikuku@strathmore.edu>
2024-09-19 13:08:51 +0300

    implemented bootstrap

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     require_once "load.php";
     $ObjLayouts->heading();
     $ObjMenus->main_menu();
+    $ObjLayouts->banner();
     $ObjContents->main_content();
     $ObjContents->sidebar();
     $ObjLayouts->footer();
-    ?>
