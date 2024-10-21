fcff9383fbef68f9bc4cbc8158bce595e9d98205
SikukuGlen <glen.sikuku@strathmore.edu>
2024-10-21 15:39:10 +0300

    Verified the verification code v2

--- global/SendMail.php
+++ global/SendMail.php
@@ -22,24 +22,24 @@ public function SendMail($mailMsg){
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'musakasamuel@gmail.com';                     //SMTP username
-        $mail->Password   = '';                               //SMTP password
+        $mail->Username   = 'gman40653@gmail.com';                     //SMTP username
+        $mail->Password   = 'jzjngwnbpmcrrmqp';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set 
     
-        //Recipients
-        $mail->setFrom('ics@gmail.com', 'ICS 2024');
-        $mail->addAddress($mailMsg['to_email'], $mailMsg['to_name']);     //Add a recipient
-    
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = $mailMsg['subject'];
-        $mail->Body    = nl2br($mailMsg['message']);
-    
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        die("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
-    }
-}
-}
+          //Recipients
+          $mail->setFrom('ics@gmail.com', 'ICS 2024');
+          $mail->addAddress($mailMsg['to_email'], $mailMsg['to_name']);     //Add a recipient
+      
+          //Content
+          $mail->isHTML(true);                                  //Set email format to HTML
+          $mail->Subject = $mailMsg['subject'];
+          $mail->Body    = nl2br($mailMsg['message']);
+      
+          $mail->send();
+          echo 'Message has been sent';
+      } catch (Exception $e) {
+          die("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+      }
+  }
+  }
